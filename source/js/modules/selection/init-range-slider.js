43fe73b9a60d4d0db7c269d81da2f812b7a46692
Diana Pavlova <diana50888@gmail.com>
2024-12-08 12:34:32 +0200

    feat: реализует рабочий слайдер в фильтре "ЛЕВЕЛ" без стилей

--- source/js/modules/selection/init-range-slider.js
+++ source/js/modules/selection/init-range-slider.js
@@ -6,6 +6,7 @@ const catalogPade = document.querySelector('[data-catalog]');
 const initRangeSlider = () => {
   if (catalogPade) {
     const sliderElement = catalogPade.querySelector('.selection__slider');
+    const rangeValue = catalogPade.querySelector('[data-range-value]');
 
     noUiSlider.create(sliderElement, {
       range: {
@@ -15,8 +16,20 @@ const initRangeSlider = () => {
       start: 30,
       step: 1,
       connect: 'upper',
+      format: {
+        to: function (value) {
+          return value;
+        },
+        from: function (value) {
+          return parseFloat(value);
+        },
+      },
     });
 
+
+    sliderElement.noUiSlider.on('update', (...rest) => {
+      rangeValue.value = sliderElement.noUiSlider.get();
+    });
   }
 };
 
